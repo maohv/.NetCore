cf252b6a5a7d10925a62adecdcdda46a21abd659
PC <maohvhn@gmail.com>
2023-05-08 22:38:37 +0700

    update CS10 CS11 PC nha

---
+++ CS10 - Lớp trong C# và lập trình hướng đối tượng/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CS10___Lớp_trong_C__và_lập_trình_hướng_đối_tượng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vukhi sungluc = new Vukhi();
+            // sungluc.name = "Sung luc";
+            // sungluc.ThietlapSatthuong(5);
+
+            // Vukhi sungmay = new Vukhi("Sung may", 15);
+            // sungluc.Tancong();
+            // sungmay.Tancong();
+
+
+
+            // for (int i = 0; i < 100000; i++)
+            // {
+            //     Student sinhvien = new Student("sinh vien: " + i);
+            //     //khi doi tuong nay dc gan = null thi ham huy se chay
+            //     sinhvien = null;
+
+            // }
+
+            //Cac cau lenh khoi using thì biến s chỉ hiệu lực trong phạm vi using, ra ngoài là giải phóng, đc thu hồi
+            using (Student s = new Student("Ten sinh vien"))
+            {
+
+            }
+        }
+    }
+}
---
+++ CS11 - Chuỗi ký tự (xâu ký tự) string và lớp StringBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StringBuilder thongbao = new StringBuilder();
+        thongbao.Append("Xin");
+        thongbao.Append(" chao cac ban");
+        thongbao.Replace("xin chao", "chao mung" );
+        string kq = thongbao.ToString();
+        Console.WriteLine(kq);
+    }
+}
