fc8e6ecc4b065c18d1cb419b4f734ab95952d6a8
{Zin} <{maohvhn@gmail.com}>
2023-05-08 19:24:40 +0700

    Update Laptop 8/5

---
+++ CS06 Rẽ nhánh với lệnh switch case C#, lệnh goto/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // int a = 3;
+
+        // switch(a)
+        // {
+        //     case 1:
+        //         Console.WriteLine($"Gia tri a bang mot");
+        //     break;
+        //     case 2:
+        //         Console.WriteLine($"Gia tri a bang hai");
+        //     break;
+        //     case 3:
+        //         Console.WriteLine($"Gia tri a bang ba");
+        //     break;
+        //     case 4:
+        //         Console.WriteLine($"Gia tri a bang bon");
+        //     break;
+
+        //     default:
+        //         Console.WriteLine("Hay thu so khac");
+        //     break;
+        // }
+
+        int a, b;
+        Console.WriteLine("Nhap so a: ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap so b: ");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hay chon lenh");
+        Console.WriteLine("1) Tinh tong");
+        Console.WriteLine("2) Tinh hieu");
+        Console.WriteLine("3) Tinh tich");
+        Console.WriteLine("4) Tinh thuong");
+
+        L1:
+        char c = Console.ReadKey().KeyChar; //Đọc kí tự được nhấn
+
+        switch(c)
+        {
+            case '1':
+            Console.WriteLine($": Tong hai so a va b la {a + b}");
+            break;
+            case '2':
+            Console.WriteLine($": Hieu hai so a va b la {a - b}");
+            break;
+            case '3':
+            Console.WriteLine($": Tich hai so a va b la {a * b}");
+            break;
+            case '4':
+            Console.WriteLine($": Thuong hai so a va b la {a / b}");
+            break;
+
+            default:
+            Console.WriteLine("Hay chon cac lenh tren");
+            goto L1;
+            break;
+        }
+    }
+}
