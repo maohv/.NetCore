f9ba4287f126f1d163d27e9260410217153ad1fd
PC <maohvhn@gmail.com>
2023-05-14 22:59:32 +0700

    update PC nha 14/5

--- CS25 - Tìm hiểu và sử dụng ngoại lệ Exception câu lệnh try catch/Program.cs
+++ CS25 - Tìm hiểu và sử dụng ngoại lệ Exception câu lệnh try catch/Program.cs
@@ -1,7 +1,70 @@
-﻿internal class Program
+﻿using MyExceptions;
+internal class Program
 {
+    static void Register(string name, int age)
+    {
+        if (string.IsNullOrEmpty(name))
+        { //nếu tên bằng null hoặc rỗng sẽ phát sinh ra 1 ngoại lệ
+            Exception exception = new Exception("Ten phai khac rong");
+            throw exception;
+        }
+        if (age < 18 || age > 100)
+        {
+            throw new AgeException(age);
+        }
+        Console.WriteLine($"Xin chao {name} {age}");
+    }
     static void Main(string[] args)
     {
+        //int a = 5;
+        //int b = 6;
+        //try
+        //{
+        //    var c = a / b;          //phat sinh doi tuong lop Expception, ke thua Expception
+        //    Console.WriteLine(c);
+        //}
+        //catch(Exception e)
+        //{
+        //    Console.WriteLine("Phep chia co loi");
+        //}
+        //Console.WriteLine("Co loi");
+
+
+
+        // string path = /*"1.txt"*/ null;
+        // try
+        // {
+        //     string s = File.ReadAllText(path);
+        //     Console.WriteLine(s);
+        // }
+        // catch (ArgumentNullException ane)
+        // {
+        //     Console.WriteLine(ane.Message);
+        //     Console.WriteLine("Duong dan file phai khac null");
+        // }
+        // catch (FileNotFoundException fnfe)
+        // {
+        //     Console.WriteLine(fnfe.Message);
+        //     Console.WriteLine("File k ton tai");
+        // }
+        // catch (Exception e)
+        // {
+        //     Console.WriteLine(e.Message);
+        // }
+
+        try
+        {
+            Register("", 20);
+        }
+        catch (NameEmtyException nee)
+        {
+            Console.WriteLine(nee.Message);
+        }
+        catch (AgeException e)
+        {
+            Console.WriteLine(e.Message);
+            e.Detail();
+        }
 
     }
 }
---
+++ CS26 - Kiểu dữ liệu danh sách List và SortedList/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int ID { get; set; }
+    public string Origin { get; set; }
+}
+internal class Program
+{
+    // List
+    // SortedList -> key/value
+    static void Main(string[] args)
+    {
+        // List<int> ds1 = new List<int>() { 4, 7, 9, 5, 6, 8, 2 };
+
+        // var rs = ds1.FindAll(
+        //     (e) =>
+        //     {
+        //         return e % 2 == 0;
+        //     }
+        // );
+        // foreach (var item in rs)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        // List<Product> products = new List<Product>(){
+        //     new Product(){
+        //         Name = "Iphone X", Price = 1000, Origin = "China", ID = 1
+        //     },
+        //       new Product(){
+        //         Name = "SamSung", Price = 900, Origin = "China", ID = 2
+        //     },
+        //       new Product(){
+        //         Name = "Sony", Price = 1100, Origin = "Japan", ID = 3
+        //     },
+        //       new Product(){
+        //         Name = "Nokia", Price = 600, Origin = "China", ID = 4
+        //     },
+        // };
+
+        // var sanphamtimdc = products.FindAll(
+        //     (p) =>
+        //     {
+        //         return p.Price <= 800;
+        //     }
+        // );
+        // foreach (var p in products)
+        // {
+        //     Console.WriteLine($"{p.Name} - {p.Price} - {p.Origin}");
+        // }
+
+        // Console.WriteLine("=======================");
+        // Console.WriteLine("Sau khi sắp xếp");
+        // products.Sort(
+        //     (p1, p2) =>
+        //     {
+        //         // 0 p1 == p2
+        //         // 1 p1 > p2
+        //         //-1 p1 < p2
+        //         if (p1.Price == p2.Price) return 1;
+        //         if (p1.Price < p2.Price) return 2;
+        //         return -1;
+        //     }
+        // );
+        // foreach (var p in products)
+        // {
+        //     Console.WriteLine($"{p.Name} - {p.Price} - {p.Origin}");
+        // }
+
+        SortedList<string, Product> products = new SortedList<string, Product>();
+        products["sanpham1"] = new Product() { Name = "Iphone X", Price = 1000, Origin = "China", ID = 1 };
+        products["sanpham2"] = new Product() { Name = "Samsung", Price = 900, Origin = "China", ID = 2 };
+        products.Add("sanpham3", new Product() { Name = "Samsung", Price = 900, Origin = "China", ID = 3 });
+
+        var p = products["sanpham2"];
+        Console.WriteLine(p.Name);
+
+        // var keys = products.Keys;
+        // var values = products.Values;
+
+        // foreach (var k in keys)
+        // {
+        //     var product = products[k];
+        //     Console.WriteLine(product.Name);
+        // }
+
+        foreach (KeyValuePair<string, Product> item in products)
+        {
+            var key = item.Key;
+            var value = item.Value;
+            Console.WriteLine($"{key} - {value.Name}");
+        }
+    }
+}
