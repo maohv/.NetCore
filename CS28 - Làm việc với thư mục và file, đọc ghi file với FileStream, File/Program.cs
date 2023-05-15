71e46932200a13225e3d967f2cad3f488b93c31b
PC <maohvhn@gmail.com>
2023-05-15 22:48:34 +0700

    update PC nha 15/5

--- CS28 - Làm việc với thư mục và file, đọc ghi file với FileStream, File/Program.cs
+++ CS28 - Làm việc với thư mục và file, đọc ghi file với FileStream, File/Program.cs
@@ -1,33 +1,117 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+
+class Product
+{
+    public int ID { get; set; }
+    public double Price { get; set; }
+    public string Name { get; set; }
+
+    public void Save(Stream stream)
+    {
+        //int -> 4 byte
+        var bytes_id = BitConverter.GetBytes(ID);
+        stream.Write(bytes_id, 0, 4);
+
+        //double -> 8 byte
+        var bytes_price = BitConverter.GetBytes(Price);
+        stream.Write(bytes_price, 0, 8);
+
+        //chuoi Name
+        var bytes_name = Encoding.UTF8.GetBytes(Name);
+        var bytes_leng = BitConverter.GetBytes(bytes_name.Length);
+        stream.Write(bytes_leng, 0, 4);
+        stream.Write(bytes_name, 0, bytes_name.Length);
+    }
+
+    public void Restore(Stream stream)
+    {
+        //int 4 byte
+        var byte_id = new byte[4];
+        stream.Read(byte_id, 0, 4);
+        ID = BitConverter.ToInt32(byte_id, 0);
+
+        //double -> 8 byte
+        var bytes_price = new byte[8];
+        stream.Write(bytes_price, 0, 8);
+        Price = BitConverter.ToDouble(bytes_price, 0);
+
+        //chuoi Name
+        var bytes_leng = new byte[4];
+        stream.Read(bytes_leng, 0, 4);
+        int leng = BitConverter.ToInt32(bytes_leng, 0);
+
+        var bytes_name = new byte[leng];
+        stream.Read(bytes_name, 0, leng);
+        Name = Encoding.UTF8.GetString(bytes_name, 0, leng);
+    }
+}
 internal class Program
 {
+    static void ListFileDirectory(string path)
+    {
+        String[] directories = Directory.GetDirectories(path);
+        String[] files = Directory.GetFiles(path);
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+        }
+        foreach (var directory in directories)
+        {
+            Console.WriteLine(directory);
+            ListFileDirectory(directory); // Đệ quy
+        }
+    }
     static void Main(string[] args)
     {
         //DriveInfo
         //Directory - Thu muc
-        var drives = DriveInfo.GetDrives(); //;ấy hết tất cả các ổ trong PC
-        // DriveInfo drive = new DriveInfo("C");
+        //Path
+        //File
 
-        // foreach (var drive in drives)
+        // string path = "obj";
+
+        // ListFileDirectory(path);
+
+
+        string path = "data.dat";
+        using var stream = new FileStream(path: path, FileMode.Open);
+
+        Product product = new Product();
         // {
-        //     Console.WriteLine($"Drive: {drive.Name}");
-        //     Console.WriteLine($"Drive Type: {drive.DriveType}");
-        //     Console.WriteLine($"Lable: {drive.VolumeLabel}");
-        //     Console.WriteLine($"Format: {drive.DriveFormat}");
-        //     Console.WriteLine($"Drive: {drive.TotalSize}");
-        //     Console.WriteLine($"Drive: {drive.TotalFreeSpace}");
-        // }
+        //     ID = 10,
+        //     Price = 12345,
+        //     Name = "San Pham Abc"
 
-        string path = "C";
-        if (Directory.Exists(path)) //kiem tra xem thu muc ton tai hay khong
-        {
-            Console.WriteLine($"Thu muc {path} ton tai");
-        }
-        else
-        {
-            Console.WriteLine($"Thu muc {path} khong ton tai");
-        }
+        // };
+
+        // product.Save(stream);
+
+        product.Restore(stream);
+
+        Console.WriteLine($"{product.Name} - {product.Price} - {product.ID}");
+
+
+        ////luu du lieu
+        //byte[] buffer = { 1, 2, 3 };
+
+        //int offset = 0;
+
+        //int count = 3;
+
+        //stream.Write(buffer, 0, 3);
+
+        ////Doc du lieu
+        //int sobytedocduoc = stream.Read(buffer, 0, 3);
+
+        //// int, double, --> byte. Su dung lop BitConverter
+
+        //int abc = 1;
+        //var byte_abc = BitConverter.GetBytes(abc);
+
+        //// bytes --> int, double
+        //BitConverter.ToInt32(byte_abc, 0);
 
     }
 }
