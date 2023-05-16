2e6601cb699f39dbd5db498cf6c5c54767e00805
PC <maohvhn@gmail.com>
2023-05-16 22:34:40 +0700

    Update Pc Nha 16/5

--- CS29 - Lập trình bất đồng bộ Asynchronousvới async await và task/Program.cs
+++ CS29 - Lập trình bất đồng bộ Asynchronousvới async await và task/Program.cs
@@ -64,17 +64,65 @@
 
         }
 
+        static async Task<string> Task4()
+        {
+            Task<string> t4 = new Task<string>(
+               () =>
+               {
+                   DoSomething(10, "T4", ConsoleColor.Green);
+                   return "Return from T4";
+               }); //tương ứng với delagate () => {return string}
+            t4.Start();
+            var kq = await t4;
+            Console.WriteLine("T4 da hoan thanh");
+            return kq;
+        }
+
+        static async Task<string> Task5()
+        {
+
+            Task<string> t5 = new Task<string>(
+                (object ob) =>
+                {
+                    string t = (string)ob;
+                    DoSomething(4, t, ConsoleColor.Yellow);
+                    return "Return from T5";
+                }, "T5");
+            t5.Start();
+            var kq = await t5;
+            Console.WriteLine("T5 da hoan thanh");
+            return kq;
+
+        }
+
+        static async Task<string> Getweb(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage kq = await httpClient.GetAsync(url);
+            string content = await kq.Content.ReadAsStringAsync();
+            return content;
+        }
         //asynchronous (multi thread)
         //asyn/await
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // //synchronous
-            // //Task
-            Task t2 = Task2();
-            Task t3 = Task3();
-            Task.WaitAll(t2, t3); //Đảm bảo các tác vụ được hoàn thành mới thực hiện đc các tác phụ tiếp theo
+            //synchronous
+            //Task
+            //Task<T>
+            Task<string> t4 = Task4();
+            Task<string> t5 = Task5();
 
+
+
+            // Task t2 = Task2();
+            // Task t3 = Task3();
+            // Task.WaitAll(t2, t3); //Đảm bảo các tác vụ được hoàn thành mới thực hiện đc các tác phụ tiếp theo
             DoSomething(6, "T1", ConsoleColor.Blue);
+            // await t2;
+            // await t3;
+
+            Task.WaitAll(t4, t5);
+
 
             Console.WriteLine("bam phim bat ky de ket thuc");
             Console.ReadKey();
