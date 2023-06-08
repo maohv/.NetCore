4ad536901a163f86b5b5572937d5c5983438d0cc
{Zin} <{maohvhn@gmail.com}>
2023-06-08 21:19:38 +0700

    update

---
+++ CS51 - (ASP.NET Razor 02) Cú pháp cơ bản trong ASP.NET Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS51____ASP.NET_Razor_02__Cú_pháp_cơ_bản_trong_ASP.NET_Razor.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
